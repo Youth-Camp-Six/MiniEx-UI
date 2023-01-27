b254e5f47b420ff88736e03aa64d5bf6b8e12193
tsja2001 <2945532754@qq.com>
2023-01-27 20:40:08 +0800

    feat: 支持深色模式:1/1

--- docs/layout/header/Header.tsx
+++ docs/layout/header/Header.tsx
@@ -1,9 +1,21 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import cls from './Header.module.less';
 import { Link } from 'react-router-dom';
 import { Button } from '../../../src';
 
 const Header: React.FC = () => {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const handleModule = () => {
+    if (themeMode === 'light') {
+      setThemeMode('mi-dark');
+      document.body.classList.add('mi-dark');
+    } else {
+      setThemeMode('light');
+      document.body.classList.remove('mi-dark');
+    }
+  };
+
   return (
     <div className={cls.header}>
       <div className='left'>
@@ -16,6 +28,7 @@ const Header: React.FC = () => {
         <Link to='/doc'>
           <Button btnType='link'>文档</Button>
         </Link>
+        <Button onClick={handleModule}>{themeMode}</Button>
       </div>
     </div>
   );
