7203d642a2cbc10baefe19b626f184cda9c6d72f
Whiskeyi <825906196@qq.com>
2023-02-08 23:32:29 +0800

    feat: 404页面优化

--- docs/views/not-found/not-found.tsx
+++ docs/views/not-found/not-found.tsx
@@ -1,11 +1,15 @@
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+
 import cls from './not-found.module.less';
 
 const NotFound: React.FC = () => {
   return (
     <div className={cls.content}>
-      <div>404</div>
-      <div>不存在此页面</div>
+      <div>404 Not Found</div>
+      <Link to='/'>
+        <div className={cls.link}>回到首页</div>
+      </Link>
     </div>
   );
 };
