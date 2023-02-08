33889caf078ef0a8c2915e8b8c7a05240f60518d
Whiskeyi <825906196@qq.com>
2023-02-09 00:52:39 +0800

    feat: header配置项迁移

--- docs/i18n.tsx
+++ docs/i18n.tsx
@@ -1,19 +1,7 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-const translationEnglish = {
-  language: 'English',
-  headerHome: 'Home',
-  headerDoc: 'Document',
-  headerdev: 'Dev',
-};
-
-const translationChinese = {
-  language: '中文',
-  headerHome: '首页',
-  headerDoc: '文档',
-  headerdev: '开发',
-};
+import { translationEnglish, translationChinese } from './layout/header/header.config';
 
 //---Using different namespaces
 const resources = {
