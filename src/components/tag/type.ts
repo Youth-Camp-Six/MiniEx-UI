0dbc979667d8c2bc7c87dc0d7ba97c1dbdb7c3de
JunlinGong <913756523@qq.com>
2023-02-13 16:51:59 +0800

    feat: 完善inputtag功能

--- src/components/tag/type.ts
+++ src/components/tag/type.ts
@@ -1,16 +1,17 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 type sizeRules = 'small' | 'medium' | 'large';
 type typeRules = 'primary' | 'info' | 'success' | 'error' | 'warning' | 'normal';
 export interface TagProps {
-    className?: string;
-    size?: sizeRules;
-    type?: typeRules;
-    maxWidth?: string;
-    round?: string;
-    closable?: boolean;
-    disabled?: string;
-    to?: string;
-    bold?: string;
-    children: ReactNode;
+  className?: string;
+  size?: sizeRules;
+  type?: typeRules;
+  maxWidth?: string;
+  round?: string;
+  closable?: boolean;
+  disabled?: string;
+  to?: string;
+  bold?: string;
+  children: ReactNode;
+  handleClick?: (e: any) => void;
 }
