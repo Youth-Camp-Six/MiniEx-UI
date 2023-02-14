be0aee17e7f4485526d9c8f6c5dde1ae1e62fd76
lorde <rioaformyking@gmail.com>
2023-02-14 15:42:33 +0800

    feat: base toast component(without doc)

---
+++ src/components/toast/Toast.tsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useRef, memo, type FC } from 'react';
+import type { ToastProps } from './type';
+
+export const Toast: FC<ToastProps> = (props) => {
+  const {
+    title,
+    message,
+    onClose,
+    timeout = 3000,
+    pauseOnHover = true,
+    position = 'bottom-right',
+    style = 'default',
+  } = props;
+
+  const [show, setShow] = useState<boolean>(true);
+  const [progress, setProgress] = useState<number>(100);
+  const [timerId, setTimerId] = useState<NodeJS.Timeout>();
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(false);
+    }, timeout);
+
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => prevProgress - 1);
+    }, timeout / 100);
+
+    setTimerId(timer);
+    setIntervalId(interval);
+
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
+  }, [timeout]);
+
+  useEffect(() => {
+    if (!show) {
+      if (onClose) {
+        onClose();
+      }
+    }
+  }, [show, onClose]);
+
+  useEffect(() => {
+    if (ref.current && pauseOnHover) {
+      const pause = () => {
+        clearInterval(intervalId);
+        clearTimeout(timerId);
+      };
+      const resume = () => {
+        const newTimer = setTimeout(() => {
+          setShow(false);
+        }, (timeout / 100) * progress);
+        const newInterval = setInterval(() => {
+          setProgress((prevProgress) => prevProgress - 1);
+        }, timeout / 100);
+        setTimerId(newTimer);
+        setIntervalId(newInterval);
+      };
+      ref.current.addEventListener('mouseenter', pause);
+      ref.current.addEventListener('mouseleave', resume);
+      return () => {
+        ref.current?.removeEventListener('mouseenter', pause);
+        ref.current?.removeEventListener('mouseleave', resume);
+      };
+    }
+  }, [pauseOnHover, progress, timerId, intervalId]);
+
+  return (
+    <div
+      ref={ref}
+      className={`toast ${show ? 'show' : 'hide'} ${position} ${style}`}
+      style={{
+        cursor: pauseOnHover ? 'pointer' : 'default',
+      }}
+    >
+      <div className='header'>
+        {title && <div className='title'>{title}</div>}
+        <button className='close-button' onClick={() => setShow(false)}>
+          <span>✕</span>
+        </button>
+      </div>
+      <div className='message'>{message}</div>
+      <div className='progressbar' style={{ width: `${progress}%` }}></div>
+    </div>
+  );
+};
+
+export default memo(Toast);
---
+++ src/components/toast/type.ts
@@ -0,0 +1,22 @@
+export type ToastPosition =
+  | 'top-left'
+  | 'top-right'
+  | 'top-center'
+  | 'bottom-left'
+  | 'bottom-right'
+  | 'bottom-center';
+
+export type TypeOptions = 'info' | 'success' | 'warning' | 'error' | 'default';
+
+export type Theme = 'light' | 'dark';
+
+export interface ToastProps {
+  title?: string;
+  message: string;
+  onClose?: () => void;
+  timeout?: number;
+  pauseOnHover?: boolean;
+  position?: ToastPosition;
+  style?: TypeOptions;
+  theme?: Theme;
+}
