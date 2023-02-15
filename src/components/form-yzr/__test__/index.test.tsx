1e92f0aa9fbfb61d2e243f4e45b1cfda17274b96
tsja2001 <2945532754@qq.com>
2023-02-16 00:23:11 +0800

    feat: 完善form组件测试代码

--- src/components/form-yzr/__test__/index.test.tsx
+++ src/components/form-yzr/__test__/index.test.tsx
@@ -3,10 +3,10 @@
  */
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Field, Form, useForm } from '..';
-import { Button, CheckboxGroup, RadioGroup } from '../../../../src';
+import { Button, CheckboxGroup, RadioGroup } from '../../../../src/components';
 
 const radioOptions = [
   { label: 'Unknown', value: '0' },
@@ -45,72 +45,51 @@ describe('test Form comonent', () => {
     expect(wrapper.container.firstChild).toMatchSnapshot();
   });
 
-  it('calls onFinishHandler when form is submitted', () => {
+  it('submits the form and calls onFinishHandler', () => {
     const onFinishHandler = jest.fn();
+    const onFinishFailedHandler = jest.fn();
 
-    const TestComponent = () => {
-      return (
-        <Form labelwidth='100px' onFinish={onFinishHandler}>
-          <Field name='sex' label='性别'>
-            <RadioGroup data-testid='radioGroup' options={radioOptions}></RadioGroup>
-          </Field>
-          <Field name='checkboxData' label='lebel'>
-            <CheckboxGroup options={checkboxOptions}></CheckboxGroup>
-          </Field>
-          <div>
-            <Button data-testid='submit'>提交</Button>
-          </div>
-        </Form>
-      );
-    };
+    const { getByLabelText, getByText } = render(
+      <Form onFinish={onFinishHandler} onFinishFailed={onFinishFailedHandler}>
+        <Field name='sex' label='性别'>
+          <RadioGroup
+            options={[
+              { label: 'Unknown', value: '0' },
+              { label: 'man', value: '1' },
+              { label: 'woman', value: '2' },
+            ]}
+          />
+        </Field>
+        <Field name='checkboxData' label='lebel'>
+          <CheckboxGroup
+            options={[
+              { label: 'labe-0', value: 0 },
+              { label: 'labe-1', value: 1 },
+              { label: 'labe-2', value: 2 },
+            ]}
+          />
+        </Field>
+        <div>
+          <Button>提交</Button>
+        </div>
+      </Form>
+    );
 
-    const { getByTestId } = render(<TestComponent />);
+    // Select radio button
+    fireEvent.click(getByLabelText('man'));
 
-    const form = getByTestId('submit');
+    // Select first and second checkbox
+    fireEvent.click(getByLabelText('labe-0'));
+    fireEvent.click(getByLabelText('labe-1'));
 
-    form.click();
+    // Submit form
+    fireEvent.click(getByText('提交'));
 
-    expect(onFinishHandler).toHaveBeenCalled();
+    // Check that the onFinishHandler function was called
+    expect(onFinishHandler).toHaveBeenCalledTimes(1);
+    expect(onFinishHandler).toBeCalledWith({
+      sex: '1',
+      checkboxData: [0, 1],
+    });
   });
-
-  it('gets form data when getFromData is called', () => {
-    // let formHook: any;
-    // const TestComponent = () => {
-    //   const [form] = useForm({
-    //     // checkboxData: [0, 1],
-    //     // sex: '0',
-    //   });
-    //   formHook = form;
-    //   return (
-    //     <Form labelwidth='100px' form={form}>
-    //       <Field name='sex' label='性别'>
-    //         <RadioGroup options={radioOptions}></RadioGroup>
-    //       </Field>
-    //       <Field name='checkboxData' label='lebel'>
-    //         <CheckboxGroup options={checkboxOptions}></CheckboxGroup>
-    //       </Field>
-    //       <div>
-    //         <Button>提交</Button>
-    //       </div>
-    //     </Form>
-    //   );
-    // };
-    // const res = render(<TestComponent />);
-    // expect(formHook.getFieldsValue()).toEqual({
-    //   checkboxData: [0, 1],
-    //   sex: '0',
-    // });
-    // const { getByTestId } = render(<TestComponent />);
-    // fireEvent.click(getFromDataButton);
-    // // You can use jest.spyOn to spy on console.log and assert that it was called with the correct arguments
-    // expect(console.log).toHaveBeenCalledWith('get form data');
-  });
-
-  // const element = wrapper.getByText('nice') as HTMLButtonElement;
-  // expect(element).toBeInTheDocument();
-  // expect(element.tagName).toEqual('BUTTON');
-  // expect(element).toHaveClass('btn btn-default');
-  // expect(element.disabled).toBeFalsy();
-  // fireEvent.click(element);
-  // expect(defaultProps.onClick).toHaveBeenCalled();
 });
