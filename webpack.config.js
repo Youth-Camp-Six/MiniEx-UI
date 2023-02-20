0b5becb30cd2731d3f0f94e2bda5e3510b2ff44d
Liny777 <1093742432@qq.com>
2023-02-20 17:42:08 +0800

    fix: webpack deal with css loader

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   plugins: [new CleanWebpackPlugin()],
