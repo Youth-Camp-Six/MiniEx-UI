ff6915cdc8ce642ffc489b19f6e06f5aae08f3aa
lorde <rioaformyking@gmail.com>
2023-02-20 19:01:26 +0800

    feat: add i18next config for docs

--- docs/i18n.tsx
+++ docs/i18n.tsx
@@ -3,13 +3,37 @@ import { initReactI18next } from 'react-i18next';
 
 import { translationEnglish, translationChinese } from './layout/header/header.config';
 
+const docUS = {
+  showCode: 'Show code',
+  hideCode: 'Hide code',
+  Param: 'Param',
+  Description: 'Description',
+  Type: 'Type',
+  Default: 'Default value',
+  Copy: 'Copy',
+  Copied: 'Copied',
+};
+
+const docCN = {
+  showCode: '展示源码',
+  hideCode: '隐藏源码',
+  Param: '参数名',
+  Description: '描述',
+  Type: '类型',
+  Default: '默认值',
+  Copy: '复制代码',
+  Copied: '代码已复制',
+};
+
 //---Using different namespaces
 const resources = {
   en: {
     home: translationEnglish,
+    doc: docUS,
   },
   zh: {
     home: translationChinese,
+    doc: docCN,
   },
 };
 
