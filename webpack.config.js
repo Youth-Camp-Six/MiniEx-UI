da354543aa5a0c552efd472231c96a355651d510
Liny777 <1093742432@qq.com>
2023-02-22 05:09:21 +0800

    feat: npm config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,93 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const tsImportPluginFactory = require('ts-import-plugin');
 module.exports = {
   mode: 'production',
   entry: './src/index.ts',
   output: {
     filename: 'miniex-ui.js',
-    path: path.resolve(__dirname, 'packages'),
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'lib'),
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.less'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.less', '.css'],
   },
   module: {
     rules: [
+      // {
+      //   test: /\.(js|ts|tsx)$/,
+      //   loader: 'ts-loader',
+      //   exclude: /node_modules/,
+      // },
+      // {
+      //   test: /\.(js|ts|tsx)$/,
+      //   loader: 'babel-loader',
+      //   options: {
+      //     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+      //   },
+      // },
       {
-        test: /\.(js|ts|tsx)$/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
       },
       {
         test: /\.less$/,
         use: [
           {
-            loader: 'style-loader',
+            loader: MiniCssExtractPlugin.loader,
           },
-          {
-            loader: 'css-loader',
-          },
+          'css-loader',
           {
             loader: 'less-loader',
-          },
-          {
-            loader: 'postcss-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
           },
         ],
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
+        // use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin()],
+  // plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin('index.css')],
+  plugins: [
+    new CleanWebpackPlugin(),
+    // new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style/index.less',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: false,
+      }),
+    ],
+  },
+  externals: ['react', 'react-dom', nodeExternals()],
 };
